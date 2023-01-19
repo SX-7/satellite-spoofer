9a783e4f8f121e52d07ebf5db19ee5c87543a065
SX_7 <sn1.test.preria.2002@gmail.com>
2023-01-19 11:32:14 +0100

    Fixed typo

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Flysat fs = new Flysat("https://www.flysat.com/en/satellitelist");
+        Webpages fs = new Flysat("https://www.flysat.com/en/satellitelist");
         ArrayList<Satellite> al = fs.getSatellites();
     }
 }
